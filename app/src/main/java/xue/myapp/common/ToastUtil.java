47d42d9011d0b09a4ee6e4c0f6833fab5e1d6e5e
897902334@qq.com <897902334@qq.com>
2017-04-17 11:18:49 +0800

    吐司工具类

---
+++ app/src/main/java/xue/myapp/common/ToastUtil.java
@@ -0,0 +1,66 @@
+package xue.myapp.common;
+
+import android.content.Context;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import xue.myapp.R;
+
+/**
+ * 作者：薛
+ * 时间：2017/4/17 09:36
+ * 吐司工具类:  单例Toast，加载布局Toast
+ */
+public class ToastUtil {
+
+    private static Toast toast;
+
+    /**
+     * 自定义Toast，加载布局显示Toast
+     */
+
+    public static void  showViewToast(Context context, CharSequence text) {
+        View v = LayoutInflater.from(context).inflate(R.layout.toast_layout, null);
+        TextView textView = (TextView) v.findViewById(R.id.textView1);
+        textView.setText(text);
+        toast = new Toast(context);
+        toast.setDuration(Toast.LENGTH_SHORT);
+        toast.setGravity(Gravity.CENTER, 0, 0);
+        toast.setView(v);
+        toast.show();
+    }
+
+
+    /**
+     * 只显示一次Toast，只改变内容,避免多次创建
+     */
+    public static void  showToast(Context context,CharSequence content){
+        if (toast==null){
+            toast=Toast.makeText(context,content,Toast.LENGTH_SHORT);
+        }else{
+            toast.setText(content);
+            toast.setDuration(Toast.LENGTH_SHORT);
+        }
+        toast.show();
+    }
+
+
+    /**
+     * 普通的Toast
+     */
+    public static void makeText(Context context,String content){
+        toast=Toast.makeText(context,content,Toast.LENGTH_SHORT);
+        toast.show();
+    }
+
+
+    //取消Toast
+    public void cancelToast() {
+        if (toast != null) {
+            toast.cancel();
+        }
+    }
+}
