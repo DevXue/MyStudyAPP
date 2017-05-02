dc3d048915d54b2d684007d1fab010f9b53c4c92
897902334@qq.com <897902334@qq.com>
2017-05-02 11:59:15 +0800

    新增Fragment

---
+++ app/src/main/java/xue/myapp/fragment/SimpleFragment.java
@@ -0,0 +1,39 @@
+package xue.myapp.fragment;
+
+import android.os.Bundle;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import xue.myapp.R;
+
+public class SimpleFragment extends Fragment {
+    private static final String ARG_PARAM1 = "info";
+
+    public static SimpleFragment newInstance(String info) {
+        SimpleFragment fragment = new SimpleFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, info);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_simple, null);
+        TextView tvInfo = (TextView) view.findViewById(R.id.textView);
+        tvInfo.setText(getArguments().getString("info"));
+        tvInfo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Snackbar.make(v, "哈喽我的", Snackbar.LENGTH_SHORT).show();
+            }
+        });
+        return view;
+    }
+}
